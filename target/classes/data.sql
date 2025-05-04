45a7a40f97b54402d00fc7a407956d95cd67ab88
mgastron <matiasgastron@hotmail.com>
2025-05-04 20:06:16 -0300

    Remove target from version control

--- target/classes/data.sql
+++
@@ -1,2 +0,0 @@
-INSERT INTO users (username, password, is_admin, email) VALUES ('admin', '1234', true, null);
-INSERT INTO users (username, password, is_admin, email) VALUES ('usuario1', '1234', false, 'miverdadyjusticia@gmail.com'); 
